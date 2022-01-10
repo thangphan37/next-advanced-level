7b9af5cd508c74627c2a1b053e012e86583ad033
ThangPhan <thang.phan@kinto-mobility.biz>
2022-01-10 15:50:07 +0900

    add rehydration

---
+++ pages/lesson-2/rehydration.tsx
@@ -0,0 +1,9 @@
+function Rehydration() {
+  if (typeof window === 'undefined') {
+    return <div>This is content of server side 😀</div>
+  }
+
+  return <div>This is the content of client side 🥶</div>
+}
+
+export default Rehydration
