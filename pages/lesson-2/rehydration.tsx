ab3cbd7bf01eea5b04b1f431bbfc294ce43832a5
ThangPhan <thang.phan@kinto-mobility.biz>
2022-01-10 15:56:33 +0900

    remove emojii

--- pages/lesson-2/rehydration.tsx
+++ pages/lesson-2/rehydration.tsx
@@ -1,9 +1,9 @@
 function Rehydration() {
   if (typeof window === 'undefined') {
-    return <div>This is content of server side 😀</div>
+    return <div>This is content of server side.</div>
   }
 
-  return <div>This is the content of client side 🥶</div>
+  return <div>This is the content of client side.</div>
 }
 
 export default Rehydration
