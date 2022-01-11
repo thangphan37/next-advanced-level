d223ec8d5696d6330d254caec326a43ced5948a5
ThangPhan <thang.phan@kinto-mobility.biz>
2022-01-11 16:57:25 +0900

    add

---
+++ pages/api/lesson-3/2.2.answer.ts
@@ -0,0 +1,16 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+const previewData = Array.from({length: 10}, (_, idx) => ({
+  id: idx,
+  userId: idx + 1,
+  title: `Post ${idx}`,
+  body: `Post Content ${idx}`,
+}))
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.query.secretToken !== 'MY_SECRET_TOKEN' && !req.query.slug) {
+    return res.status(400).json({message: 'Invalid Token!'})
+  }
+  res.setPreviewData(previewData)
+  res.redirect(req.query.slug as string)
+}
